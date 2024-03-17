bfcca3a826e8f9dce7267fba9b5a79ab0b40334e
ahlaiptek <iptekahla@gmail.com>
2024-03-17 23:15:00 +0700

    The first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const notesCointainer = document.querySelector(".notes-container");
+const createButton = document.getElementById("input-box");
+
+let notes = document.querySelectorAll(".input-box")
