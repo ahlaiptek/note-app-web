ac3deb700c27b847f82b9905a495d47bca48b18e
ahlaiptek <iptekahla@gmail.com>
2024-03-17 23:24:25 +0700

    Selesai

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const notesCointainer = document.querySelector(".notes-container");
-const createButton = document.getElementById("input-box");
+const createButton = document.getElementById("create-button");
 
-let notes = document.querySelectorAll(".input-box")
+let notes = document.querySelectorAll(".input-box");
+
+createButton.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+
+    img.src = "assets/delete.png";
+    notesCointainer.appendChild(inputBox).appendChild(img);
+});
+
+notesCointainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+    }
+});
